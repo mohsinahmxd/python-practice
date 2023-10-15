6d6e063c17307c21c84a4912423f3fa863bfe86f
Mohsin <mohsin-ahmed01@outlook.com>
2023-10-15 17:26:20 +0100

    practice if statements

---
+++ if.py
@@ -0,0 +1,12 @@
+# if statements practice
+
+book = 'boring'
+
+if book == 'good':
+    print("This book is great!")
+elif book == 'boring':
+    print("This book is boring!")
+else:
+    print("Unkown")
+
+print("Done")
