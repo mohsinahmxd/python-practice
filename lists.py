191206811b64909646ce80cc2283f24475487b45
Mohsin <mohsin-ahmed01@outlook.com>
2023-10-15 17:48:08 +0100

    practiced more lists

--- lists.py
+++ lists.py
@@ -4,3 +4,12 @@ print(nums[0])
 print(nums[0:3])
 print(nums[-2:]) # last 2 elements
 print(nums[1:])
+
+nums.append(6)
+nums.insert(2, 'x')
+print(nums)
+nums.remove('x')
+print(nums)
+
+print('x' in nums)
+print(len(nums))
